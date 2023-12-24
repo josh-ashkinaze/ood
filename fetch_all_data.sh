a584bd08d33b9d98d4525f8f2aad95517feb9ae7
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-12-24 09:24:54 -0500

    increase data period and amnt fetched

--- fetch_all_data.sh
+++ fetch_all_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 my_start_date='2017-01-01'
-my_end_date='2023-01-01'
+my_end_date='2024-01-01'
 
 python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date"
 python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date"
