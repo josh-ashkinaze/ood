ee05e1890af68ed6873c845b837fbecbce86638f
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2024-01-30 15:51:09 -0500

    add pod script

--- 1_fetch_actual_data.sh
+++ 1_fetch_actual_data.sh
@@ -7,4 +7,6 @@ python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date"
 python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date"
 python3 fetch_osf_preprints.py --provider 'socarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 3000
 python3 fetch_osf_preprints.py --provider 'psyarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 3000
+python3 fetch_podcasts  --start_date "$my_start_date" --end_date "$my_end_date" --N 50000
+
 echo "All done!"
