6fe0333e5917ff32d4c3699b6b3497898f72ee84
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2024-02-06 13:49:19 -0500

    bash

--- 0_fetch_pilot_data.sh
+++ 0_fetch_pilot_data.sh
@@ -8,8 +8,9 @@ my_end_date='2023-03-01'
 
 python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
 python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
+python3 fetch_nyt_opeds.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
+python3 fetch_podcasts  --start_date "$my_start_date" --end_date "$my_end_date" --N 50000
 python3 fetch_osf_preprints.py --provider 'socarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 500 --pilot
 python3 fetch_osf_preprints.py --provider 'psyarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 500 --pilot
-python3 fetch_nyt_opeds.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
 
 echo "All done!"
