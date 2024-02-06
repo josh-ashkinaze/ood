a287bab8d3da597ccf3d7ab24a00ab6081a36ef4
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2024-02-06 12:22:20 -0500

    two months

--- 0_fetch_pilot_data.sh
+++ 0_fetch_pilot_data.sh
@@ -4,7 +4,7 @@
 # and prompt engineering.
 
 my_start_date='2023-01-01'
-my_end_date='2023-02-01'
+my_end_date='2023-03-01'
 
 python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
 python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
