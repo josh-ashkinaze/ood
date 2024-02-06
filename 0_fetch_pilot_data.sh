cc8c6bc14c65eba9d381a1eff3dd0031ecb38046
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2024-02-06 14:21:00 -0500

    comment out

--- 0_fetch_pilot_data.sh
+++ 0_fetch_pilot_data.sh
@@ -6,7 +6,7 @@
 my_start_date='2023-01-01'
 my_end_date='2023-03-01'
 
-python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
+#python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
 python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
 python3 fetch_nyt_opeds.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
 python3 fetch_podcasts  --start_date "$my_start_date" --end_date "$my_end_date" --pilot --N 50000
