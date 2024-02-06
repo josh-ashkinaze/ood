883999c32d27919921883f16d95b8d9a87a4cdff
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2024-02-06 14:41:45 -0500

    temp podcasts

--- 0_fetch_pilot_data.sh
+++ 0_fetch_pilot_data.sh
@@ -6,11 +6,11 @@
 my_start_date='2023-01-01'
 my_end_date='2023-03-01'
 
-python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
-python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
-python3 fetch_nyt_opeds.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
-python3 fetch_podcasts  --start_date "$my_start_date" --end_date "$my_end_date" --pilot --N 50000
-python3 fetch_osf_preprints.py --provider 'socarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 500 --pilot
-python3 fetch_osf_preprints.py --provider 'psyarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 500 --pilot
+#python3 fetch_fiction.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
+#python3 fetch_startups.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
+#python3 fetch_nyt_opeds.py --start_date "$my_start_date" --end_date "$my_end_date" --pilot
+python3 fetch_podcasts.py  --start_date "$my_start_date" --end_date "$my_end_date" --pilot --N 5000
+#python3 fetch_osf_preprints.py --provider 'socarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 500 --pilot
+#python3 fetch_osf_preprints.py --provider 'psyarxiv' --start_date "$my_start_date" --end_date "$my_end_date" --max_results_per_month 500 --pilot
 
 echo "All done!"
